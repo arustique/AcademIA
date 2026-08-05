f15604dbe44b34ece21ab1c093c3f15d17074533
arustique <arustique@gmail.com>
2026-08-05 02:51:55 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -7,6 +7,6 @@
 window.ACADEMIA_CONFIG = {
   SUPABASE_URL: "https://chyboeyrtoqvwgztevke.supabase.co/rest/v1/",
   SUPABASE_ANON_KEY: "sb_publishable_-_sQslwKnyrUZHYxEKINkA_JNEqx1Ri",
-  CHARIOW_CHECKOUT_URL: "https://chariow.com/VOTRE-LIEN-DE-PAIEMENT",
+  CHARIOW_CHECKOUT_URL: "https://igsdecng.mychariow.shop/prd_hae4deth",
   PRICE_LABEL: "10 000 FCFA",
 };
